1f61bd149e4ea7462e6bffd292f11fdb58dabe25
LuXuryPro <LuXuryPro@users.noreply.github.com>
2016-05-16 22:43:50 +0200

    Fix

--- Projekt.pl
+++ Projekt.pl
@@ -1,42 +1,62 @@
-between(Min, Max, Return) :- Cur is Min, Min < Max.
-between(Min, Max, Min).
-between(Min, Max, Value) :- Cur is Min + 1, Cur < Max, between(Cur, Max, Value).
+% global variables list
+variables([a,b,c,d,e,f,g,h,i,j,k,l,k,n,o,p,q,r,s,t,u,v,w,x,y,z]).
 
+between(Min, _, Min).
+between(Min, Max, Value) :-
+    Cur is Min + 1,
+    Cur =< Max,
+    between(Cur, Max, Value).
 
-pick_from_table_at([X|R], 0, X).
-pick_from_table_at([X|R], J, Value) :-
+pick_from_table_at([X|_], 1, X).
+pick_from_table_at([_|R], J, Value) :-
 	NewJ is J - 1,
 	pick_from_table_at(R, NewJ, Value).
 
-
-
-/** Wybranie zmiennej użytej w następniku lub we wcześniej zbudowanym wyrażeniu, w poprzedniku - zakres indeksu od 1 do LastUsed; flag na 1 */
-insert_arg(LastUsed, LastLocal, FlagIn, Value, LastLocal, yes),
+/**
+ * insert_arg:
+ *
+ * - LastUsed - indeks ostatnio użytej zmennej w poprzednich wyrażeniach
+ * - LastLocal - indeks ostatnio użytej zmennej
+ * - FlagIn - czy wybrano do tej pory zmenną użytą poprzednio
+ * - Value - zwrócona zmienna
+ * - LastLocal - zwrócony największy indeks zmiennej użytej lokalnie
+ * - FlagIn - czy udało się wybrać zmienną użytą poprzednio
+ *
+ * Wybranie zmiennej użytej w następniku lub we wcześniej zbudowanym wyrażeniu,
+ * w poprzedniku - zakres indeksu od 1 do LastUsed; flag na 1
+ */
+insert_arg(LastUsed, LastLocal, FlagIn, Value, LastLocal, yes) :-
 	between(1, LastUsed, Index),
-	variable(Table),
+	variables(Table),
 	pick_from_table_at(Table, Index, Value).
 
-/** W przeciwnym przypadku przepisuję flagę */
+/**
+ * W przeciwnym przypadku przepisuję flagę
+ */
 insert_arg(LastUsed, LastLocal, FlagIn, Value, LastLocal, FlagIn) :-
 	NewLastUsed is LastUsed + 1,
 	between(NewLastUsed, LastLocal, Index),
-	variable(Table),
+	variables(Table),
 	pick_from_table_at(Table, Index, Value).
 
-/** Wybranie nowej zmiennej - zwiększenie LastLocal o 1 i pobraniu zmiennej */
-insert_arg(LastUsed, LastLocal, FlagIn, Value, LastLocal, FlagIn) :-
+/**
+ * Wybranie nowej zmiennej - zwiększenie LastLocal o 1 i pobraniu zmiennej
+ */
+insert_arg(LastUsed, LastLocal, FlagIn, Value, NewLastLocal, FlagIn) :-
 	NewLastLocal is LastLocal + 1,
-	variable(Table),
-	pick_from_table_at(Table, Index, Value).
-
+	variables(Table),
+	pick_from_table_at(Table, NewLastLocal, Value).
 
 
 build_arg_list(N, vars(LastUsed, LastLocal), Flag, [Arg|Rest], RetLastUsed) :-
-	M is N + 1,
-	M > 1,
+	N > 1,
 	insert_arg(LastUsed, LastLocal, Flag, Arg, RetLastLocal, FlagOut),
+    M is N - 1,
 	build_arg_list(M, vars(LastUsed, RetLastLocal), FlagOut, Rest, RetLastUsed).
-build_arg_list(1, vars(LastUsed, LastLocal), yes, [LastArg|Rest], RetLastUsed) :-
-	insert_arg(LastUsed, LastLocal, yes, Arg, LastArg, FlagOut).
 
+build_arg_list(1, vars(LastUsed, LastLocal), yes, [Arg], RetLastUsed) :-
+	insert_arg(LastUsed, LastLocal, yes, Arg, RetLastUsed, FlagOut).
+
+build_arg_list(1, vars(LastUsed, LastLocal), no, [Arg], RetLastUsed) :-
+	insert_arg(LastUsed, LastLocal, no, Arg, RetLastUsed, yes).
 
